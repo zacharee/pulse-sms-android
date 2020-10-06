2df6eb01443d1194bf38a9db5196b87d09d0a3de
Zachary Wander <zachary.wander@gmail.com>
2020-10-06 15:13:07 -0400

    Rename knownSpam word-related stuff to use scoring terms

--- SMSFilter/src/main/java/tk/zwander/smsfilter/Database.kt
+++ SMSFilter/src/main/java/tk/zwander/smsfilter/Database.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import java.io.File
 
 class Database(private val context: Context, private val targetFile: File) {
-    private val knownSpamKeywords = HashMap<String, Int>()
+    private val keywordScores = HashMap<String, Int>()
     private val reportedSpamMessages = HashMap<String, Int>()
 
     //TODO: There might be a better collection for this.
@@ -18,30 +18,26 @@ class Database(private val context: Context, private val targetFile: File) {
         return 0
     }
 
-    fun addWordToSpamDatabase(word: String, overrideScore: Int = -1) {
+    fun updateWordScore(word: String, overrideScore: Int = -1) {
         val score = if (overrideScore > -1)
             overrideScore else checkKeywordScore(word)
 
-        knownSpamKeywords[word] = score
+        keywordScores[word] = score
     }
 
     fun addMessageToSpamDatabase(msg: String) {
         reportedSpamMessages[msg] = msg.split(" ")
                 .map {
                     checkKeywordScore(it).also { score ->
-                        addWordToSpamDatabase(it, score)
+                        updateWordScore(it, score)
                     }
                 }.sum()
     }
 
     fun addMessageToGoodDatabase(msg: String) {
         //TODO: It might make sense to either lessen the scores of or
-        //TODO: remove any words from knownSpamKeywords, as well.
+        //TODO: remove any words from keywordScores, as well.
         knownGoodMessages.add(msg)
-    }
-
-    fun removeWordFromSpamDatabase(word: String) {
-        knownSpamKeywords.remove(word)
     }
 
     fun removeMessageFromSpamDatabase(msg: String) {
